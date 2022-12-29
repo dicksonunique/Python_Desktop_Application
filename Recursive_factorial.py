a1101d4503e3a1012838d0358eb9b5bc70bdb594
Codeimpressive <57103201+dicksonunique@users.noreply.github.com>
2022-12-29 14:50:26 +0100

    Update Recursive_factorial.py

--- Recursive_factorial.py
+++ Recursive_factorial.py
@@ -1,7 +1,7 @@
 """Program to calculate factorial recursively"""
 def factorial(num):
     result = 1
-    if num == 0 or num == 1:
+    if num <= 0 or num == 1:
         return 1
     for i in range(num):
         result = num * factorial(num-1)
