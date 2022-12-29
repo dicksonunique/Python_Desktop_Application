c979b5c1090604b4848f4ab695baf4fa2bf42fbe
Codeimpressive <57103201+dicksonunique@users.noreply.github.com>
2022-12-29 16:41:16 +0100

    Update TowerOfHanoi.py

--- TowerOfHanoi.py
+++ TowerOfHanoi.py
@@ -3,7 +3,7 @@ def TowerOfHanoi(Num_of_disc, move_from, move_to, assist_bar):
     if Num_of_disc == 0:
         return
     TowerOfHanoi(Num_of_disc - 1, move_from, assist_bar, move_to )
-    print(f"Move {Num_of_disc}  from {move_from} to {move_to }")
+    print(f"Move disc {Num_of_disc}  from {move_from} to {move_to }")
     TowerOfHanoi(Num_of_disc - 1, assist_bar, move_to, move_from )
     
 if __name__ == '__main__':
@@ -15,3 +15,5 @@ if __name__ == '__main__':
         print(f"{numberOfDisc} is not a number")
     
     
+    
+
