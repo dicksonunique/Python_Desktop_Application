15f867f702355fd6bc1fb96a06daae2f357550ac
Codeimpressive <57103201+dicksonunique@users.noreply.github.com>
2022-12-29 16:34:39 +0100

    Create TowerOfHanoi.py

---
+++ TowerOfHanoi.py
@@ -0,0 +1,17 @@
+""" Tower of Hanoi moving all disc to bar B"""
+def TowerOfHanoi(Num_of_disc, move_from, move_to, assist_bar):
+    if Num_of_disc == 0:
+        return
+    TowerOfHanoi(Num_of_disc - 1, move_from, assist_bar, move_to )
+    print(f"Move {Num_of_disc}  from {move_from} to {move_to }")
+    TowerOfHanoi(Num_of_disc - 1, assist_bar, move_to, move_from )
+    
+if __name__ == '__main__':
+    numberOfDisc = input('Enter number of disc')
+    if numberOfDisc.isdigit():
+        num = int(numberOfDisc)
+        TowerOfHanoi(num,'A','B','C' )
+    else:
+        print(f"{numberOfDisc} is not a number")
+    
+    
